2efcaf8f1c66a99b7026a79631a4f4f14a5c92c4
Dennis Hakvoort <1521006+DennisHakvoort@users.noreply.github.com>
2018-05-09 12:02:19 +0200

    BR9 added

--- Business rules.sql
+++ Business rules.sql
@@ -25,3 +25,27 @@ EXEC SP_DROP_CONSTRAINT @Constraint_name = 'CK_UREN_MIN_MAX', @tablename = 'mede
 ALTER TABLE medewerker_beschikbaarheid
 		ADD CONSTRAINT CK_UREN_MIN_MAX CHECK (beschikbaar_uren < 184 AND beschikbaar_uren > 0)
 
+-- BR9 BR9 De waarden van project, medewerker op project en medewerker_ingepland_project
+-- kunnen niet meer worden aangepast als project(eind_datum) is verstreken,
+
+ALTER TRIGGER TG_PROJECT_VERSTREKEN_PROJECT
+	ON project
+	AFTER INSERT, UPDATE, DELETE
+	AS
+	BEGIN
+		SELECT *
+		FROM (project P INNER JOIN inserted I ON P.project_code = I.project_code) LEFT OUTER JOIN deleted D on P.project_code = D.project_code
+		WHERE (I.eind_datum < CURRENT_TIMESTAMP OR D.eind_datum < CURRENT_TIMESTAMP)
+
+		IF EXISTS(SELECT '!'
+							FROM (project P INNER JOIN inserted I ON P.project_code = I.project_code) LEFT OUTER JOIN deleted D on P.project_code = D.project_code
+							WHERE (I.eind_datum < CURRENT_TIMESTAMP OR D.eind_datum < CURRENT_TIMESTAMP)
+
+			THROW 50001, 'Een project kan niet meer aangepast worden nadat deze is afgelopen.', 16
+	END
+
+BEGIN TRANSACTION
+INSERT INTO project_categorie VALUES ('d', NULL)
+INSERT INTO project VALUES (1, 'd', '15 jan 2018', '22 feb 2018', 'testerdetest')
+ROLLBACK TRANSACTION
+
