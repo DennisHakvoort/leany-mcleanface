e3923835b9220301abb1875f57ef521b9ffd25c2
hyhyn <hyhng97@gmail.com>
2018-05-09 09:36:29 +0200

    ging iets mis met de vorige commit

--- Business rules.sql
+++ Business rules.sql
@@ -5,5 +5,6 @@
  *	BR5 Medewerker_ingepland_project(medewerker_uren) kan niet minder zijn dan 0
  *	BR6 Medewerker_ingepland_project(medewerker_uren) kan niet meer zijn dan 184
 */
+
 ALTER TABLE MEDEWERKER_INGEPLAND_PROJECT WITH CHECK
 	ADD CONSTRAINT CK_UREN_MIN_MAX CHECK (medewerker_uren > 0 and medewerker_uren < 184)
