d850db35023f97af608866cbff2c8a7adb685153
Dennis Hakvoort <1521006+DennisHakvoort@users.noreply.github.com>
2018-05-14 14:23:36 +0200

    kritieke bug gefixt in de spelling.

--- Business rules.sql
+++ Business rules.sql
@@ -40,7 +40,7 @@ ALTER TABLE medewerker_beschikbaarheid
 --BR3
 --medewerker(medewerker_code) bestaat uit de eerste letter van de voornaam,
 --de eerste letter van de achternaam en
---een volgnummer dat met ��n verhoogd wanneer de medewerker code al bestaat.
+--een volgnummer dat met één verhoogd wanneer de medewerker code al bestaat.
 GO
 CREATE PROCEDURE sp_MedewerkerToevoegen
 					@achternaam CHAR(20), @voornaam CHAR(20)
