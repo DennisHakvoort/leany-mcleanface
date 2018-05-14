38f6443d334fd7645319516b5967ec3b0484e032
hyhyn <hoang_97@hotmail.com>
2018-05-14 14:04:56 +0200

    br 3 verplaatst

--- Business rules.sql
+++ Business rules.sql
@@ -3,6 +3,33 @@
 USE LeanDb
 GO
 
+--PROCEDURE OM CONSTRAINTS TE DROPPEN ALS DEZE BESTAAN
+GO
+CREATE PROCEDURE SP_DROP_CONSTRAINT
+	@Constraint_name VARCHAR(255) = NULL,
+	@tablename VARCHAR(255) = NULL
+	AS
+	BEGIN TRY
+		declare @sql NVARCHAR(255)
+    SELECT @sql = 'ALTER TABLE ' + @tablename + ' DROP CONSTRAINT ' + @Constraint_name;
+		EXEC sys.sp_executesql @stmt = @sql
+	END TRY
+	BEGIN CATCH
+		PRINT 'Het volgende constraint is niet gedropt, waarschijnlijk omdat deze niet bestond: ' + @Constraint_name
+	END CATCH
+	GO
+
+--DROP ALL BUSINESS RULES
+EXEC SP_DROP_CONSTRAINT @Constraint_name = 'CK_UREN_MIN_MAX', @tablename = 'medewerker_beschikbaarheid'
+EXEC SP_DROP_CONSTRAINT @Constraintname = 'CK_EINDDATUM_NA_BEGINDATUM', @tablename = 'project'
+EXEC SP_DROP_CONSTRAINT @Constraint_name = 'CK_EINDDATUM_NA_BEGINDATUM', @tablename = 'project'
+DROP PROC spProjecturenInplannen
+
+--BR1 Medewerker_beshikbaar(beschikbaar_uren) kan niet meer zijn dan 184
+--BR2 Medewerker_beshikbaar(beschikbaar_uren) kan niet minder zijn dan 0
+ALTER TABLE medewerker_beschikbaarheid
+		ADD CONSTRAINT CK_UREN_MIN_MAX CHECK (beschikbaar_uren < 184 AND beschikbaar_uren > 0)
+
 --BR3
 --medewerker(medewerker_code) bestaat uit de eerste letter van de voornaam, 
 --de eerste letter van de achternaam en 
@@ -46,33 +73,6 @@ RAISERROR (@ERROR_MESSAGE, @ERROR_SEVERITY, @ERROR_STATE )
 END CATCH
 END
 GO
-
---PROCEDURE OM CONSTRAINTS TE DROPPEN ALS DEZE BESTAAN
-GO
-CREATE PROCEDURE SP_DROP_CONSTRAINT
-	@Constraint_name VARCHAR(255) = NULL,
-	@tablename VARCHAR(255) = NULL
-	AS
-	BEGIN TRY
-		declare @sql NVARCHAR(255)
-    SELECT @sql = 'ALTER TABLE ' + @tablename + ' DROP CONSTRAINT ' + @Constraint_name;
-		EXEC sys.sp_executesql @stmt = @sql
-	END TRY
-	BEGIN CATCH
-		PRINT 'Het volgende constraint is niet gedropt, waarschijnlijk omdat deze niet bestond: ' + @Constraint_name
-	END CATCH
-	GO
-
---DROP ALL BUSINESS RULES
-EXEC SP_DROP_CONSTRAINT @Constraint_name = 'CK_UREN_MIN_MAX', @tablename = 'medewerker_beschikbaarheid'
-EXEC SP_DROP_CONSTRAINT @Constraintname = 'CK_EINDDATUM_NA_BEGINDATUM', @tablename = 'project'
-EXEC SP_DROP_CONSTRAINT @Constraint_name = 'CK_EINDDATUM_NA_BEGINDATUM', @tablename = 'project'
-DROP PROC spProjecturenInplannen
-
---BR1 Medewerker_beshikbaar(beschikbaar_uren) kan niet meer zijn dan 184
---BR2 Medewerker_beshikbaar(beschikbaar_uren) kan niet minder zijn dan 0
-ALTER TABLE medewerker_beschikbaarheid
-		ADD CONSTRAINT CK_UREN_MIN_MAX CHECK (beschikbaar_uren < 184 AND beschikbaar_uren > 0)
 
 -- BR5 Medewerker_ingepland_project(medewerker_uren) kan niet minder zijn dan 0
 -- BR6 Medewerker_ingepland_project(medewerker_uren) kan niet meer zijn dan 184
