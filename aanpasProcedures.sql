4b76ca552c53de62bdd8bee007589fa240439bf4
Jarco <jarcodavelaar@gmail.com>
2018-05-23 16:52:59 +0200

    procedure en test gemaakt voor het wijzigen van medewerker rol typen.

--- aanpasProcedures.sql
+++ aanpasProcedures.sql
@@ -44,3 +44,43 @@ AS
 			END;
 		THROW
 	END CATCH
+
+			--SP aanpassen medewerker rol types
+CREATE PROCEDURE sp_WijzigMedewerkerRolType
+@medewerker_Rol_Oud   CHAR(40),
+@medewerker_Rol_Nieuw CHAR(40)
+AS
+	SET NOCOUNT ON
+	SET XACT_ABORT OFF
+	DECLARE @TranCounter INT;
+	SET @TranCounter = @@TRANCOUNT;
+	SELECT @TranCounter
+	IF @TranCounter > 0
+		SAVE TRANSACTION ProcedureSave;
+	ELSE
+		BEGIN TRANSACTION;
+	BEGIN TRY
+	BEGIN
+		IF NOT EXISTS (SELECT medewerker_rol
+				   FROM medewerker_rol_type
+				   WHERE medewerker_rol = @medewerker_Rol_Oud)
+		THROW 50008, 'medewerker rol bestaat niet.', 16
+		END
+	UPDATE medewerker_rol_type
+	SET medewerker_rol = @medewerker_Rol_Nieuw
+	WHERE medewerker_rol = @medewerker_Rol_Oud
+	END TRY
+	BEGIN CATCH
+		IF @TranCounter = 0
+			BEGIN
+				PRINT'ROLLBACK TRANSACTION'
+				IF XACT_STATE() = 1 ROLLBACK TRANSACTION;
+			END;
+		ELSE
+			BEGIN
+				PRINT'ROLLBACK TRANSACTION PROCEDURESAVE'
+				PRINT XACT_STATE()
+        IF XACT_STATE() <> -1 ROLLBACK TRANSACTION ProcedureSave;
+			END;
+		THROW
+	END CATCH
