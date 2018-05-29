779e670b9c0c808171b4cb8d28afd82b10db69b9
Rick Zweers <rickgz66@gmail.com>
2018-05-29 16:39:12 +0200

    feedback hoang verwerkt
    nutteloze select onderin verwijderd, drop view if exists toegevoegd

--- Views.sql
+++ Views.sql
@@ -1,6 +1,12 @@
 USE LeanDb
 GO
 
+DROP VIEW IF EXISTS vw_Bezetting
+DROP VIEW IF EXISTS vw_Totaal_Gepland_Beschikbaar_Jaar
+DROP VIEW IF EXISTS vw_Project_Overzicht_Bezetting
+DROP VIEW IF EXISTS vw_Actief_Project_Percentage_Gedekte_Uren
+
+GO
 --VIEW1 Bezettingspercentages per maand per medewerker
 CREATE VIEW vw_Bezetting AS
 SELECT j.medewerker_code, year(maand_datum) AS jaar,
@@ -26,60 +32,12 @@ FROM (SELECT mop.medewerker_code, maand_datum, sum(mip.medewerker_uren) totaal_u
 WHERE beschikbare_dagen > 0 AND totaal_uren > 0
 GROUP BY j.medewerker_code, year(maand_datum)
 
-
-
--- SELECT p.medewerker_code, year(i.maand_datum) AS jaar,
--- isnull(case when month(i.maand_datum) = 1 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Jan,
--- isnull(case when month(i.maand_datum) = 2 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Feb,
--- isnull(case when month(i.maand_datum) = 3 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Mar,
--- isnull(case when month(i.maand_datum) = 4 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Apr,
--- isnull(case when month(i.maand_datum) = 5 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Mei,
--- isnull(case when month(i.maand_datum) = 6 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Jun,
--- isnull(case when month(i.maand_datum) = 7 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Jul,
--- isnull(case when month(i.maand_datum) = 8 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Aug,
--- isnull(case when month(i.maand_datum) = 9 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Sep,
--- isnull(case when month(i.maand_datum) = 10 then (CAST(i.medewerker_uren AS FLOAT)/ b.beschikbaar_uren) * 100 end, 0) Okt,
--- isnull(case when month(i.maand_datum) = 12 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Nov,
--- isnull(case when month(i.maand_datum) = 13 then (CAST(i.medewerker_uren AS FLOAT) / b.beschikbaar_uren) * 100 end, 0) Dec
--- FROM medewerker_op_project p LEFT OUTER JOIN medewerker_beschikbaarheid b ON p.medewerker_code = b.medewerker_code
--- 							 LEFT OUTER JOIN medewerker_ingepland_project i ON p.id = i.id
--- WHERE b.beschikbaar_uren IS NOT NULL AND b.beschikbaar_uren > 0
--- GROUP BY p.medewerker_code, i.medewerker_uren, b.beschikbaar_uren, i.maand_datum
-
-
-
-/*
---TODO: REMOVE TEST DATA IF OTHER DATA HAS BEEN GENERATED.
---TEST DATA BEGINT HIER
-INSERT INTO project_categorie VALUES
-	('Onderwijs', NULL),
-	('Middelbaar onderwijs', 'Onderwijs'),
-	('Facturering', NULL)
-
-INSERT INTO project VALUES
-	(1, 'Onderwijs', '12 jan 2017', '29 dec 2018', 'Onderwijs project'),
-	(2, 'Facturering', '3 apr 3018', '22 aug 2019', 'Succesvol factureren')
-
-INSERT INTO project_rol_type VALUES
-	('Projectleider'),
-	('Medewerker')
-
-INSERT INTO medewerker VALUES
-	('JP', 'Jan', 'Pieter'),
-	('CS', 'Chris', 'Scholten'),
-	('SC', 'Santa', 'Claus')
-
-INSERT INTO medewerker_beschikbaarheid VALUES
-	()
-
---TEST DATA EINDIGT HIER
-*/
-
 --Views om projectbezetting in te zien
 
 --Onderstaande view geeft weer hoeveel dagen een medewerker beschikbaar is en hoeveel uren een medewerker is ingepland per jaar. 
 --Hiermee wordt ook een percentage berekent dat aangeeft hoeveel procent van de beschikbare tijd gebruikt wordt.
 GO
+
 CREATE VIEW vw_Totaal_Gepland_Beschikbaar_Jaar
 AS
 SELECT		mb.medewerker_code, mip.jaar AS jaar, SUM(mb.beschikbare_dagen) * 8 /*werkuren in dag*/ AS totaal_beschikbaar_jaar, medewerker_uren AS totaal_ingepland_jaar, (CAST(mip.medewerker_uren AS FLOAT) / NULLIF(SUM(mb.beschikbare_dagen) * 8, 0) * 100) AS percentage_ingepland_beschikbaar
@@ -104,6 +62,7 @@ FROM		project p
 			RIGHT JOIN medewerker_ingepland_project mip ON mip.id = mop.id
 			RIGHT JOIN vw_Totaal_Gepland_Beschikbaar_Jaar vtgbj ON vtgbj.jaar = YEAR(mip.maand_datum) AND vtgbj.medewerker_code = mop.medewerker_code
 GROUP BY	mop.medewerker_code, YEAR(mip.maand_datum), p.project_code, p.project_naam, vtgbj.totaal_ingepland_jaar, vtgbj.totaal_beschikbaar_jaar, vtgbj.percentage_ingepland_beschikbaar
+
 GO
 
 --View voor vergelijken van geplande uren en geschatte benodigde uren voor een project
@@ -115,7 +74,3 @@ FROM		project p
 			INNER JOIN medewerker_ingepland_project mip ON mip.id = mop.id
 WHERE		p.eind_datum >= GETDATE()
 GROUP BY	p.project_code, p.project_naam, p.begin_datum, p.eind_datum, p.verwachte_uren
-
---project SNHd2018 SNHdYSNofxr heeft 1132 verwacht en 660 gepland
---klopt vergeleken met query hieronder
-select SUM(medewerker_uren) from medewerker_ingepland_project mip INNER JOIN medewerker_op_project mop ON mip.id = mop.id WHERE project_code = 'SNHd2018' GROUP BY project_code
