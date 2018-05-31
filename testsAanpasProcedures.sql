863cd2f1eed4a5f0e6c45874f207b2bcbbf35e99
Samir <32687845+MugiwaraSama@users.noreply.github.com>
2018-05-31 11:26:19 +0200

    test voor SP 8 aangepast
    De tests geupdate zodat het nu werkt zoals aangepaste stored procedures en daarnaast zullen de tests in de toekomst ook niet meer fallen, want de ingevoerde begin en einddatum zijn dynamisch.

--- testsAanpasProcedures.sql
+++ testsAanpasProcedures.sql
@@ -108,39 +108,40 @@ GO
 --Succes test
 BEGIN TRANSACTION
 BEGIN TRY
-	IF (select IDENT_CURRENT('medewerker_op_project')) IS NOT NULL
-	DBCC CHECKIDENT ('medewerker_op_project', RESEED,1);
+	DECLARE @maand_beschikbaar DATETIME = (GETDATE() + 40);
+
 	INSERT INTO medewerker VALUES ('cod95', 'Gebruiker7', 'Achternaam7');
-	INSERT INTO medewerker_beschikbaarheid VALUES ('cod95', 'jan 2019', 12);
+	INSERT INTO medewerker_beschikbaarheid VALUES ('cod95', @maand_beschikbaar, 12);
 	INSERT INTO medewerker_rol_type VALUES ('DeaTeacher');
 	INSERT INTO medewerker_rol VALUES ('cod95', 'DeaTeacher');
 	INSERT INTO project_categorie VALUES ('HAN Arnhem', null);
 	INSERT INTO project_categorie VALUES ('DEA_project', 'HAN Arnhem');
 	INSERT INTO categorie_tag VALUES ('school');
 	INSERT INTO tag_van_categorie VALUES ('DEA_project', 'school');
-	INSERT INTO project VALUES ('DEA12', 'DEA_project', '11 jan 2019', '11 dec 2019', 'DEA_project_2018', 320);
+	INSERT INTO project VALUES ('DEA12', 'DEA_project', GETDATE() + 30, GETDATE() + 200, 'DEA_project_2018', 320);
 	INSERT INTO project_rol_type VALUES ('CEO');
 	INSERT INTO medewerker_op_project VALUES ('DEA12', 'cod95', 'CEO');
-	INSERT INTO medewerker_ingepland_project VALUES (IDENT_CURRENT('medewerker_op_project'), 300, 'jan 2019');
-	INSERT INTO medewerker_ingepland_project VALUES (IDENT_CURRENT('medewerker_op_project'), 100, 'feb 2019');
+	INSERT INTO medewerker_ingepland_project VALUES (IDENT_CURRENT('medewerker_op_project'), 300, @maand_beschikbaar);
 	
-	DECLARE @id int = IDENT_CURRENT('medewerker_op_project');
-	EXEC sp_WijzigenMedewerkerIngeplandProject @id, 211, 'jan 2019';
-	SELECT * FROM medewerker_ingepland_project WHERE id = IDENT_CURRENT('medewerker_op_project')
+	DECLARE @id int = IDENT_CURRENT('medewerker_op_project') + 1;
+	EXEC sp_WijzigenMedewerkerIngeplandProject @id, 50, @maand_beschikbaar;
 END TRY
 	BEGIN CATCH
-		SELECT 'test mislukt' as 'resultaat', ERROR_MESSAGE() as 'error message', ERROR_NUMBER() AS 'error number', ERROR_SEVERITY() as 'error severity'
 	END CATCH
 ROLLBACK TRANSACTION
 GO
 
 --Een medewerker_ingepland_project wijzigen die niet bestaat
 --Faal test
---Msg 50080, Level 16, State 16, Procedure sp_WijzigenMedewerkerIngeplandProject, Line 20 [Batch Start Line 141]
---Er bestaat geen medewerker_ingepland_project record met de opgegeven id
+--Msg 50034, Level 16, State 16, Procedure sp_WijzigenMedewerkerIngeplandProject, Line 23 [Batch Start Line 137]
+--Er bestaat geen medewerker_ingepland_project record met de opgegeven id.
 BEGIN TRANSACTION
-	SELECT * FROM medewerker_ingepland_project
-	DECLARE @id int = IDENT_CURRENT('medewerker_op_project');
-	EXEC sp_WijzigenMedewerkerIngeplandProject @id, 200, 'jan 2018';
+BEGIN TRY
+	DECLARE @id int = IDENT_CURRENT('medewerker_op_project') + 1;
+	DECLARE @maand_beschikbaar DATETIME = (GETDATE() + 10);
+	EXEC sp_WijzigenMedewerkerIngeplandProject @id, 200, @maand_beschikbaar;
+END TRY
+	BEGIN CATCH
+	END CATCH
 ROLLBACK TRANSACTION
 GO
